f0fcb35580327f2a09bbc1dcca885dffb252f132
Hunter Bacot <hsbacot@gmail.com>
2026-01-30 17:22:53 -0800

    chore: update packs to v0.3.5 (config in help)

--- Formula/packs.rb
+++ Formula/packs.rb
@@ -4,21 +4,21 @@
 class Packs < Formula
   desc "Agent skill package manager - share and install AI agent skills"
   homepage "https://packs.sh"
-  version "0.3.4"
+  version "0.3.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/tunajam/packs/releases/download/v0.3.4/packs_0.3.4_darwin_amd64.tar.gz"
-      sha256 "da14ddcb97b19e0dcc1f3098b359cbc6127589915ffefc30bbf31bcce10989d2"
+      url "https://github.com/tunajam/packs/releases/download/v0.3.5/packs_0.3.5_darwin_amd64.tar.gz"
+      sha256 "da1b2651d99c460318cb353af890c4a708553574316ce2458ac945c739f62019"
 
       def install
         bin.install "packs"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/tunajam/packs/releases/download/v0.3.4/packs_0.3.4_darwin_arm64.tar.gz"
-      sha256 "e7e735f9ee093e04189ba52d4ffbf72e53ebf581999f219819507b1f276ac4cc"
+      url "https://github.com/tunajam/packs/releases/download/v0.3.5/packs_0.3.5_darwin_arm64.tar.gz"
+      sha256 "ea8821e120537e43adce0ccee45acda414595bb2636cd486201c12cd227fff34"
 
       def install
         bin.install "packs"
@@ -28,15 +28,15 @@ class Packs < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/tunajam/packs/releases/download/v0.3.4/packs_0.3.4_linux_amd64.tar.gz"
-      sha256 "f360344b6e144c319b81882739d4d2b6aa8517efe7618bb21ce014944f11fe0c"
+      url "https://github.com/tunajam/packs/releases/download/v0.3.5/packs_0.3.5_linux_amd64.tar.gz"
+      sha256 "72fb385b76be35766b91d293ca122c4ac32f7936f612fb41653e80a6a3fcb2c1"
       def install
         bin.install "packs"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/tunajam/packs/releases/download/v0.3.4/packs_0.3.4_linux_arm64.tar.gz"
-      sha256 "b4e127761ffe4b481b63abf3c4868581b7e173bc147ad620c9a04c22b4de7dfc"
+      url "https://github.com/tunajam/packs/releases/download/v0.3.5/packs_0.3.5_linux_arm64.tar.gz"
+      sha256 "4e8a2cd7c25ac85c49b9f4671072193af9eeb1c336b794a518c8e912e6191305"
       def install
         bin.install "packs"
       end
